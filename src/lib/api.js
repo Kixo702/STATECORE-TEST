4ceef15965e5388cc65d5fae7caef0f28f3dc081
Kixo702 <d1danik@yandex.ru>
2026-07-21 20:10:07 +0300

    обнова

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = (import.meta.env.VITE_API_BASE || 'http://localhost:4000/api').replace(/\/$/, '')
+const API_BASE = (import.meta.env.VITE_API_BASE || 'https://statecore-api.onrender.com/api').replace(/\/$/, '')
 
 export async function api(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
