eabd67ba64162011dd682100eb6c0c0eb1a98fa6
Kixo702 <d1danik@yandex.ru>
2026-07-23 16:27:17 +0300

    fix

--- src/components/Organizations.jsx
+++ src/components/Organizations.jsx
@@ -142,7 +142,7 @@ const BO_ORG_NAMES = {
 
 export default function Organizations({ user }) {
   const GOV_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/1pYaxNrSm37hydzEyLNuQsYOHF4jTfClDoJbqbSCkk2M/export?format=csv'
-  const BO_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1VeCFjnEockkuf3EFBZPnVAOfWqe8NjadnUv-2XyF2z12R_EwzPXzhr91nYK0pbcTYSTfAvkJp6co/pub?gid=1404478710&single=true&output=csv'
+  const BO_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRE89xZb9RxVOSfXbtQ4-fyu-FH9r-5ntI4AdPI6xPqmzRh0jVYd9qITXDCpWCEC0RFptElukEjhvD5/pub?gid=0&single=true&output=csv'
   const GOV_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyhZYSsvPt0QdbyYiAEfvyfu8XVQwOPeYapuG0HwV8CCngctz43msP9K_o4C-ck13Hy/exec'
   const BO_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbza0Kft9sI27q3OQtotzItO8unNViHN5yi8H8RU7xElghaSbOjJ9HYdak7u8J_XYvTH/exec'
 
