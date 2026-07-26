5dbd4f23e0b8b559710d62c8f3ed4663be7c4922
Kixo702 <d1danik@yandex.ru>
2026-07-26 22:10:23 +0300

    fix

--- src/components/Organizations.jsx
+++ src/components/Organizations.jsx
@@ -149,7 +149,7 @@ export default function Organizations({ user }) {
   const FLORIDA_MAFIA_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTW7Q7m29fHn6u9D-GYSHWe4NiXuK2ld2K8MOGEWQitE7jUzrTWi1aBl_ud6FbX2Vtl5ERZm9V-MvtC/pub?gid=0&single=true&output=csv'
   const FLORIDA_MAFIA_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqWwbrvdN4NWjebG-3NolDdrrRJd0ohBP2M620ZWG1lcibYVa6pYRB2cI_4T2XBYSu/exec'
   const FLORIDA_BIKERS_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS0DhHN9TtdXH3xeRm_0Wec4P55UmQhAbLBOuLPLAGulKMMNnTFh3M8ORmmxwPxNeiPD60ba6cb0qr2/pub?gid=0&single=true&output=csv'
-  const FLORIDA_BIKERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx11_GUytgircqNS04s0VpUzT__WCqlHuzNci8eGov0uS7y8-6vSWCUyxTJLFQ064HkEg/exec'
+  const FLORIDA_BIKERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxt34qyUDYBqNg4kmQaFvSMCLIa2VSpdlSkG_fEGdIeHMULJwGDGQNVC_iFrXn4aP94lQ/exec'
 
   const [serverId, setServerId] = useState(READY_SERVER_ID)
   const [sphereId, setSphereId] = useState('gov')
